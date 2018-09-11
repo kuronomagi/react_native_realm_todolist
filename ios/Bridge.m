dbce9a0c943207248511531c91c8541dabeb5f97
kazu <kazu20120801@gmail.com>
2018-09-11 12:56:43 +0900

    Native swift

---
+++ ios/Bridge.m
@@ -0,0 +1,15 @@
+//
+//  ridge.m
+//  tutorialProject
+//
+//  Created by 岡田和峻 on 2018/09/11.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCT_EXTERN_MODULE(HelloWorld, NSObject)
+
+RCT_EXTERN_METHOD(say)
+
+@end
---
+++ ios/HellowWorld.swift
@@ -0,0 +1,9 @@
+//
+//  HellowWorld.swift
+//  tutorialProject
+//
+//  Created by 岡田和峻 on 2018/09/11.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+import Foundation
